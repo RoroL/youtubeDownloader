acc427893f083a234b5ac9d77ed6d3b151d8cd0d
Ronan Legardinier <ronan.legardinier@gmail.com>
2014-10-02 22:59:41 +0200

    Ajout d'un compteur de fichier téléchargées

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def Bold(output):
     os.system("tput sgr 0")
 
 start = time.time()
+countFiles = 0
 
 parser = argparse.ArgumentParser()
 parser.add_argument("url", help="Youtube's playlist's url")
@@ -28,7 +29,8 @@ os.system("rm .wgetstdout")
 sourceCode = open(".sourceCode", "r")
 for line in re.findall("<ol class=\"playlist-videos-list .*</ol>", sourceCode.read(), re.DOTALL):
     for link in re.findall('/watch?.{14}', line):
-        Redbold("\nDownloading " + link + " ...")
+        countFiles += 1
+        Redbold("\n" + str(countFiles) + "/" + str(len(re.findall('/watch?.{14}', line))) + ": Downloading " + link + " ...")
         os.system("youtube-dl -i -x -o \"%(title)s.%(ext)s\" http://www.youtube.com" + link)
 
 os.system("rm .sourceCode")
