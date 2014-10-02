5fafe918aac719e2073474246f6fc25a1ad53186
Ronan Legardinier <ronan.legardinier@gmail.com>
2014-10-02 22:58:17 +0200

    Ajout des fonctions de modification de l'affichage en sortie standard

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import argparse
 import re
 import os
+
+def Redbold(output):
+    os.system("tput bold")
+    os.system("tput setaf 1")
+    print(output)
+    os.system("tput sgr 0")
+    
+def Bold(output):
+    os.system("tput bold")
+    print(output)
+    os.system("tput sgr 0")
+
 parser = argparse.ArgumentParser()
 parser.add_argument("url", help="Youtube's playlist's url")
 args = parser.parse_args()
 
-print("\nDownloading source code from url...")
-print("Source code downloaded\nDownloading files...")
+Redbold("\nDownloading source code from url...")
+Bold("Source code downloaded\nDownloading files...")
 os.system("wget -o .wgetstdout \"" + args.url + "\" -O .sourceCode")
 os.system("rm .wgetstdout")
 
 sourceCode = open(".sourceCode", "r")
 for line in re.findall("<ol class=\"playlist-videos-list .*</ol>", sourceCode.read(), re.DOTALL):
     for link in re.findall('/watch?.{14}', line):
-        print("\nDownloading " + link + " ...")
+        Redbold("\nDownloading " + link + " ...")
         os.system("youtube-dl -i -x -o \"%(title)s.%(ext)s\" http://www.youtube.com" + link)
 
 os.system("rm .sourceCode")
